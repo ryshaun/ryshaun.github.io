bf4673ce15f5a13486d3e66392e3438085c5045b
CSSI 2015 <CSSI@CSSIs-MacBook-Air.local>
2015-07-21 17:19:25 -0500

    adding files

---
+++ my_script.js
@@ -0,0 +1,9 @@
+var temperature = 80;
+var idealTemperature = 60;
+
+while ( temperature > idealTemperature ) {
+  temperature = temperature - 1;
+  console.log(temperature);
+}
+
+console.log("Ac off its Freeezing");
